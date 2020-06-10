7ab351c840c83aa8e919a6d96f6ab7e4c2f3fedd
FOXTEO2714 <65261945+FOXTEO2714@users.noreply.github.com>
2020-06-09 20:38:19 -0600

    Get
    Actualizacion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormularioComponent } from './components/formulario/formulario.component';
+import { VisorComponent } from './components/visor/visor.component';
+import { BotonAddComponent } from './components/boton-add/boton-add.component';
+import { BotonDelComponent } from './components/boton-del/boton-del.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormularioComponent,
+    VisorComponent,
+    BotonAddComponent,
+    BotonDelComponent
   ],
   imports: [
     BrowserModule,
